759a287c5b69fa2133e3b43dd10bc0c9ed78f9b2
Fadill <66961192+0Fadill@users.noreply.github.com>
2022-06-22 06:26:57 +0000

    back to before, google sheet api has a limit apparently

--- priceget.py
+++ priceget.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import priceget
 
 def get(name):
   response_API = requests.get('https://nasfaq.biz/api/getMarketInfo?all&saleValue=false&inCirculation=false')
@@ -10,5 +11,5 @@ def get(name):
   price = parse_json['coinInfo']['data'][name]['price']
   coinname = parse_json['coinInfo']['data'][name]['coin']
   
-  print("Coin name:", coinname)
-  print("Price :", price)
+
+
